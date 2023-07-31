edb419448c0dff8ffd971d3a7c1cd3b79a3034a7
sushmitha-reddy06 <ellalasushmitha06@gmail.com>
2023-07-31 11:48:08 +0530

    upadted th folder calculator2

--- calculator2/script.js
+++ calculator2/script.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     let screen = document.querySelector('.screen');
     let numberButtons = document.querySelectorAll('.btn-grey');
     let operatorButtons = document.querySelectorAll('.btn-yellow');
@@ -40,6 +40,8 @@
     }
 
     function calculateResult() {
+    console.log("Inside cal function")
+
         if (currentInput !== '' && currentOperator !== '') {
             let operands = currentInput.split(currentOperator);
             let num1 = parseFloat(operands[0]);
@@ -68,53 +70,81 @@
         }
     }
 
-    numberButtons.forEach(function(button) {
-        button.addEventListener('click', function(e) {
+    numberButtons.forEach(function (button) {
+        button.addEventListener('click', function (e) {
             handleNumberClick(e.target.textContent);
         });
     });
 
-    operatorButtons.forEach(function(button) {
-        button.addEventListener('click', function(e) {
+    operatorButtons.forEach(function (button) {
+        button.addEventListener('click', function (e) {
             handleOperatorClick(e.target.textContent);
         });
     });
 
-    equal.addEventListener('click', function(e) {
+    equal.addEventListener('click', function (e) {
         calculateResult();
         updateScreen(currentInput);
     });
 
-    clear.addEventListener('click', function(e) {
+    clear.addEventListener('click', function (e) {
         clearScreen();
     });
 
-    backspace.addEventListener('click', function(e) {
+    backspace.addEventListener('click', function (e) {
         currentInput = currentInput.slice(0, -1);
         updateScreen(currentInput);
     });
 
-    // Event listener for keyboard input using event delegation
-    document.addEventListener('keydown', function(e) {
+    // Event listener for keyboard input
+    document.addEventListener('keydown', function (e) {
         const key = e.key;
-        if (/^[0-9\+\-\*\/\.]$/.test(key)) {
-            // Numbers and operators
-            if (key === '.') {
+        console.log(key)
+        console.log(key === 'Enter')
+        // Check if the pressed key is a number, operator, or special key
+        if (/^[0-9\+\-\*\/\.\=]|Enter$|^Backspace$|^Delete$/.test(key)) {
+           
+
+            console.log("INSIDE FIRST IF CONDITION")
+
+            if (key === '=') {
+                // Equal sign
+                calculateResult();
+                updateScreen(currentInput);
+            } else if (key === 'Enter') {
+
+                console.log("Inside enter CONDITION")
+                // Ensure Enter key works as equal sign
+                calculateResult();
+               
+                updateScreen(currentInput);
+            } else if (key === 'Backspace') {
+                // Backspace
+                currentInput = currentInput.slice(0, -1);
+                updateScreen(currentInput);
+            } else if (key === '.') {
                 // Ensure only one decimal point in the input
                 if (!currentInput.includes('.')) {
                     handleNumberClick('.');
                 }
-            } else {
-                handleNumberClick(key);
             }
-        } else if (key === 'Enter') {
-            // Equal sign
-            calculateResult();
-            updateScreen(currentInput);
-        } else if (key === 'Backspace') {
-            // Backspace
-            currentInput = currentInput.slice(0, -1);
-            updateScreen(currentInput);
+            else if (key === 'Delete') {
+                clearScreen();
+                
+            } else {
+                // Numbers and operators
+                if (key === '*') {
+                    handleOperatorClick(key);
+                } else if (key === '/') {
+                    handleOperatorClick(key);
+                } else if (key === '+') {
+                    handleOperatorClick(key);
+                } else if (key === '-') {
+                    handleOperatorClick(key);
+                } else {
+                    handleNumberClick(key);
+                }
+            }
         }
     });
 })();
